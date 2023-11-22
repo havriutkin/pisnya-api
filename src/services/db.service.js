5a0d684a750f3949c29c0871df0c870c308dbfcf
havriutkin <havriutkin@gmail.com>
2023-11-22 05:36:11 -0500

    Make params empty array by default

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -7,7 +7,7 @@ const db = require("../configs/db.config");
 */
 
 // Makes quiery to the db. If data doesn't exist returns empty array
-const query = async (sql, params) => {
+const query = async (sql, params = []) => {
     const rows = await db.query(sql, params);
     const data = emptyOrRows(rows);
     return data;
