243a919aec77711c6d33565d51efc1dc1bc653fb
havriutkin <havriutkin@gmail.com>
2023-11-23 01:57:28 -0500

    Return only rows now

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -9,9 +9,9 @@ const {emptyOrRows} = require("../utils/helper.util");
 
 // Makes quiery to the db. If data doesn't exist returns empty array
 const query = async (sql, params = []) => {
-    const rows = await db.query(sql, params);
-    const data = emptyOrRows(rows);
-    return data;
+    const data = await db.query(sql, params);
+    const rows = emptyOrRows(data['rows']);
+    return rows;
 };
 
 module.exports = {
