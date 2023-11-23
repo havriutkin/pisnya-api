fb0a2f8d26e7b1b5debe50a3e460658ec37c7690
havriutkin <havriutkin@gmail.com>
2023-11-23 01:11:25 -0500

    Add router for get artist by name

--- src/routes/artist.route.js
+++ src/routes/artist.route.js
@@ -7,6 +7,8 @@ router.get('/', artistController.getAll);
 
 router.get('/:id', artistController.getById);
 
+router.get('/artistbyname', artistController.getByName);
+
 router.post('/', artistController.create);
 
 module.exports = router;
