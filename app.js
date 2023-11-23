b174cc7ed6988aa2866b03d9298e41379584e5c7
havriutkin <havriutkin@gmail.com>
2023-11-23 00:56:23 -0500

    Clean up

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
+/*
+    TODO:
+
+    3. Implement routes for songs
+    4. Implement pages
+    5. SET UP MULTER
+    6. Read about bodyParser
+*/
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./src/configs/db.config");
+const multer = require('multer');
+
+
+const artistRouter = require("./src/routes/artist.route");
 
 
 const app = express();
+const upload = multer();
 const port = 3000;
 
-app.use(bodyParser.json())
+
+/* ------------ Parsing Middleware ------------ */
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
+app.use(upload.array()); // For parsing multipart/form-data
 
+/* ------------ Static Middleware ------------ */
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/v1/', (req, res) => {
-    res.json({info: 'Hello world!'});
+
+
+/* ------------ Use routers ------------ */
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Ok'});
 })
 
+app.use('/artist', artistRouter);   // Attach artist router
+
+
+/* ------------ Error Handler Middleware ------------ */
+
+app.use((err, req, res, next) => {
+    console.log(err); // Log the error
+    res.status(500).send('Something broke!');
+});
+
+// Start server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
